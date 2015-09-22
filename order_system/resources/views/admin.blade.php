31cb6ec57e9f429fb9e0685adaf293f77dcc37f9
Adam <admpyle@gmail.com>
2015-09-22 10:13:24 +1000

    Statistics Page Added some basic metric cards to the Statistics page of the Admin Panel We should be able to add live/current database statistics when add data has been seeded.

--- order_system/resources/views/admin.blade.php
+++ order_system/resources/views/admin.blade.php
@@ -10,15 +10,76 @@
             <li class="tab col s3"><a href="#employeemgnt">Employee Manager</a></li>
         </ul>
     </div>
+
+    <!-- System Statistics -->
     <div id="stats" class="col s12">
-        Statistics go here
+      <!--card stats start-->
+      <div id="system-stats">
+        <div class="row">
+          <div class="col s12 m6 l3">
+            <div class="card">
+              <div class="card-content  green white-text">
+                <p class="card-stats-title"><i class="mdi-social-group-add"></i> Orders Today</p>
+                <h4 class="card-stats-number">566</h4>
+                <p class="card-stats-compare"><i class="mdi-hardware-keyboard-arrow-up"></i> 15% <span class="green-text text-lighten-5">from yesterday</span>
+                </p>
+              </div>
+              <div class="card-action  green darken-2">
+                <div id="clients-bar"></div>
+              </div>
+            </div>
+          </div>
+          <div class="col s12 m6 l3">
+            <div class="card">
+              <div class="card-content blue-grey white-text">
+                <p class="card-stats-title"><i class="mdi-action-trending-up"></i> Today's Sales</p>
+                <h4 class="card-stats-number">$806.52</h4>
+                <p class="card-stats-compare"><i class="mdi-hardware-keyboard-arrow-up"></i> 80% <span class="blue-grey-text text-lighten-5">from yesterday</span>
+                </p>
+              </div>
+              <div class="card-action blue-grey darken-2">
+                <div id="profit-tristate"></div>
+              </div>
+            </div>
+          </div>
+          <div class="col s12 m6 l3">
+            <div class="card">
+              <div class="card-content purple white-text">
+                <p class="card-stats-title"><i class="mdi-editor-attach-money"></i>Monthly Sales</p>
+                <h4 class="card-stats-number">$8990.63</h4>
+                <p class="card-stats-compare"><i class="mdi-hardware-keyboard-arrow-up"></i> 70% <span class="purple-text text-lighten-5">last month</span>
+                </p>
+              </div>
+              <div class="card-action purple darken-2">
+                <div id="sales-compositebar"></div>
+
+              </div>
+            </div>
+          </div>
+          <div class="col s12 m6 l3">
+            <div class="card">
+              <div class="card-content pink lighten-2 white-text">
+                <p class="card-stats-title"><i class="mdi-editor-insert-drive-file"></i> Customers</p>
+                <h4 class="card-stats-number">1806</h4>
+                <p class="card-stats-compare"><i class="mdi-hardware-keyboard-arrow-down"></i> 3% <span class="deep-purple-text text-lighten-5">from last month</span>
+                </p>
+              </div>
+              <div class="card-action  pink darken-2">
+                <div id="invoice-line"></div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <!--card stats end-->
     </div>
     <div id="custmgnt" class="col s12">
         Have list of customers with pagenation. Possibly Load on tab hit(save bandwidth)
     </div>
-    <div id="menuedit" class="col s12">
-        <p>List all menu items. Tabular form</p>
 
+    <!-- Menu Editor-->
+    <div id="menuedit" class="col s12">
         <table class="stripped">
             <thead>
                 <tr>
