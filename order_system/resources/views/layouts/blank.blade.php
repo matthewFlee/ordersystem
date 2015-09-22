b7c9dcaa44abebe6f4c6f0cb13e3daac49e92b18
Adam <admpyle@gmail.com>
2015-09-22 11:01:28 +1000

    Nav links added Added Links from Master template to blank template

--- order_system/resources/views/layouts/blank.blade.php
+++ order_system/resources/views/layouts/blank.blade.php
@@ -27,7 +27,10 @@
                 <div class="nav-wrapper">
                 <a href="#" class="brand-logo">Order System</a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="/">Home</a></li>
+                  <li><a href="/">Home</a></li>
+                  <li><a href="/">Customers</a></li>
+                  <li><a href="/">Menu</a></li>
+                  <li><a href="./admin">Admin</a></li>
                 </ul>
                 </div>
             </nav>
