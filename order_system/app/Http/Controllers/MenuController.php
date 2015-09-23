997fb92834eb80e93e62995713391a1300cd80f3
Adam Pyle <admpyle@gmail.com>
2015-09-23 17:00:48 +1000

    Menu Store function Added code for store function. This adds the request to the Menu table in the database

---
+++ order_system/app/Http/Controllers/MenuController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace OrderSystem\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use OrderSystem\Http\Requests;
+use OrderSystem\Http\Controllers\Controller;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //Returns index of menu, for general viewing
+        //return view('menu');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $item = new Menu();
+        $item->item = $input['item'];
+        $item->price = floatval($input['price']);
+        $item->save();
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //Remove a menu item
+    }
+}
