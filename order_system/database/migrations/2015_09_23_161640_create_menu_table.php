ce81385bec224f3cf6a6cba16e71acdc8872bbe7
Adam Pyle <admpyle@gmail.com>
2015-09-23 16:50:32 +1000

    Menu db migration Added migration file for menu table

---
+++ order_system/database/migrations/2015_09_23_161640_create_menu_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function(Blueprint $table){
+          $table->increments('id')->unique();
+          $table->string('item');
+          //price is name, 7 is numebr of digits, 2 is numebr after decimal
+          //e.g Format is 1000000.12
+          $table->double('price', 7,2);
+          //adds 2 colums, created_at and updated_at
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('menu');
+    }
+}
